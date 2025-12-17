a164abaf4b1b8df024616bc4ee0c9dbe60b2cf82
tazz <tazz@tazz.cloud>
2025-12-17 01:22:40 -0500

    feat(conf/wezterm): ✨ update config

--- conf/wezterm/wezterm.lua
+++ conf/wezterm/wezterm.lua
@@ -1,4 +1,29 @@
 local wt = require("wezterm")
-return {
-  default_prog = { "/opt/homebrew/bin/zellij" },
-}
+local config = wt.config_builder()
+config.initial_cols = 120
+config.initial_rows = 28
+config.font_size = 14
+
+config.color_scheme = "flexoki-dark"
+config.default_prog = { "/opt/homebrew/bin/zellij" }
+config.font = wt.font({
+  family = "Monaspace Argon Var",
+  style = "Normal",
+  harfbuzz_features = {
+    "+liga",
+    "+ital",
+    "+ss01",
+    "+ss02",
+    "+ss03",
+    "+ss04",
+    "+ss05",
+    "+ss06",
+    "+ss07",
+    "+ss08",
+    "+ss09",
+    "+ss10",
+  },
+})
+config.window_background_opacity = 0.7
+config.default_prog = { "/opt/homebrew/bin/zellij", "attach", "main" }
+return config
