19950f482daf44fd132fe451f7fc2701d7e8ed33
tazz <tazz@tazz.cloud>
2026-01-05 05:45:15 -0500

    refactor(install.sh): 🔨 cleanup install script a bit

--- install.sh
+++ install.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env sh
-INSTALL_PREFIX="${INSTALL_PREFIX:-/usr/local/share}"
-DOTBOT_INSTALL_DIR="$INSTALL_PREFIX/dotbot"
-DOTFILES_INSTALL_DIR="$INSTALL_PREFIX/dotfiles"
+INSTALL_PREFIX="${INSTALL_PREFIX:-/usr/local}"
+DOTBOT_INSTALL_DIR="$INSTALL_PREFIX/share/dotbot"
+DOTFILES_INSTALL_DIR="$INSTALL_PREFIX/share/dotfiles"
 
-# 1. Ensure Homebrew is installed
-if ! command -v brew >/dev/null 2>&1; then
-  echo "installing Homebrew..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# 2. Install utilities
-brew install \
-  git \
-  go-task/tap/go-task
+brew_install_silently() {
+  brew install \
+    "$@" \
+    >/dev/null
+}
 
 shallow_clone() {
   git clone \
@@ -24,12 +19,36 @@ shallow_clone() {
     "$2"
 }
 
+install_dotfiles() {
+  export DOTBOT_HOME="$DOTBOT_INSTALL_DIR"
+  export PATH="$DOTBOT_INSTALL_DIR/bin:$PATH"
+  # TODO(@s0cks): use task for this?
+  dotbot -d "$DOTFILES_INSTALL_DIR/" -c "$DOTFILES_INSTALL_DIR/install.yml"
+}
+
+# 0. Login as root
+sudo -v
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
+
+# 1. Ensure homebrew is installed
+if ! command -v brew >/dev/null 2>&1; then
+  echo "installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# 2. Install utilities using homebrew
+brew_install_silently \
+  git \
+  eget \
+  go-task/tap/go-task
+
 # 3. Install dotbot
 shallow_clone "s0cks/dotbot" "$DOTBOT_INSTALL_DIR/"
-
-# 4. Clone & Install dotfiles
+# 4. Clone dotfiles
 shallow_clone "s0cks/dotfiles" "$DOTFILES_INSTALL_DIR/"
-export DOTBOT_HOME="$DOTBOT_INSTALL_DIR"
-export PATH="$DOTBOT_INSTALL_DIR/bin:$PATH"
-# TODO(@s0cks): use task for this?
-dotbot -d "$DOTFILES_INSTALL_DIR/" -c "$DOTFILES_INSTALL_DIR/install.yml"
+# 5. Install dotfiles using dotbot
+install_dotfiles
